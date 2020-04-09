d81400ba0dadb5d1a56504d98216db489d89df30
Bosco <nzibosco@gmail.com>
2020-04-08 21:04:02 -0400

    registration action

---
+++ src/action-mappers/register.ts
@@ -0,0 +1,32 @@
+import {registerAPI} from '../utils/API/baseUrls';
+
+export const registerTypes = {
+    SUCCESS_REGISTRATION_SUCCESSFULL: 'SUCCESSFULL_REGISTRATION',
+    FAIL_REGISTRATION_FAIL: 'FAILED_REGISTRATION'
+
+}
+
+export const register =  (member:any) => async (dispatch:any) => {
+
+    registerAPI.post(member).then(res => {
+        if(res.status === 200){
+            console.log(res.data);
+            dispatch({
+                type: registerTypes.SUCCESS_REGISTRATION_SUCCESSFULL,
+                payload: {
+                    registerMessage: 'Registration successfull. A committee member will approve your registration shortly'
+                }
+            })
+        } else {
+            console.log(res);
+            dispatch({
+                type: registerTypes.FAIL_REGISTRATION_FAIL,
+                payload: {
+                    registerMessage: 'Registration failed. Try again soon'
+                }
+            })
+            
+        }
+    })
+
+}
